0533013f85d1a1ee3b3b97a6945ddf3d062cc88f
viqqib <faqihzaky43@gmail.com>
2024-08-14 17:38:38 +0800

    adding searching bar

--- src/app/movie/[movieTitle]/page.js
+++ src/app/movie/[movieTitle]/page.js
@@ -1,3 +1,27 @@
+'use client'
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+
 export default function MovieDetails( { params } ) {
-    return <h1>Deadpool {params.movieTitle}</h1>
+
+    function decodeText(encodedText) {
+        return decodeURIComponent(encodedText);
+    }
+      
+    const fetchDetailsMovieFromOmdb = async () => {
+        const apiKey = '4d08b29'
+        const title = decodeText(params.movieTitle)
+        const response = await axios.get(`https://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
+        return response.data;
+    }
+
+    const { data, error, refetch, isError, isLoading } = useQuery({
+        queryKey: ['omdbData'],
+        queryFn: fetchDetailsMovieFromOmdb
+    });
+
+
+
+    return  <h1>{data?.Title}</h1>
 }
