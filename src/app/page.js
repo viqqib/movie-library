e26e2a03f3db959feb6e7494f8290317cefef39c
viqqib <faqihzaky43@gmail.com>
2024-08-20 10:37:13 +0800

    set value env

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import {FaChevronCircleLeft, FaChevronCircleRight} from 'react-icons/fa'
 import NewsGlass from "@/components/NewsGlass";
 import MovieCardSlider from "@/components/MovieCardSlider";
 
+const tmdbApiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 const fetchPopularFromTmdb = async () => {
-  const tmdbApiKey = "92b0f3d01f07b3710e21b8f604bf0646";
   const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdbApiKey}`);
   return response.data.results;
 };
@@ -86,29 +86,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/* <div className="popular-movie-container">
-          <div className="flex items-center">
-            <div className="text-xl border-red-500 border-l-4 h-4"></div>
-            <p className="md:text-lg ml-2">Popular Movies</p>
-          </div>
-           
-          <div className="relative flex items-center    mt-3">
-            <FaChevronCircleLeft size={35} className="hidden md:block opacity-30 hover:opacity-80 duration-300 cursor-pointer absolute z-10 left-1 top-32" onClick={slideLeft}/>
-            <div className=" space-x-3 md:space-x-5 overflow-y-hidden overflow-x-scroll scroll  scrollbar-hide scroll-smooth flex" id="slider">
-              {popularMovies.map((movie, index) => (
-                <MovieCard
-                  alt={movie.title}
-                  key={index}
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  title={movie.title}
-                  link={`details/${movie.id}`}
-
-                />
-              ))}
-              <FaChevronCircleRight size={35} className="hidden md:block opacity-30 hover:opacity-90 duration-300 absolute z-10 right-2 top-32 cursor-pointer" onClick={slideRight}/>
-            </div>
-          </div>
-        </div> */}
+       
 
         
       </div>
